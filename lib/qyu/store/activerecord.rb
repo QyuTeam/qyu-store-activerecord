db55c3c773cc57cc11e5e3ea00e52e3c1c799d83
Andrew Kumanyaev <me@zzet.org>
2017-11-16 13:26:38 +0300

    Init gem structure

---
+++ lib/qyu/store/activerecord.rb
@@ -0,0 +1,9 @@
+require "qyu/store/activerecord/version"
+
+module Qyu
+  module Store
+    module Activerecord
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/qyu/store/activerecord/version.rb
@@ -0,0 +1,7 @@
+module Qyu
+  module Store
+    module Activerecord
+      VERSION = "0.1.0"
+    end
+  end
+end
